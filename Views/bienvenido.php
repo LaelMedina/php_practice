96dba1d8a6ce1889e1db54ed4f4f052e582b1c1c
LaelMedina <medinalael10@gmail.com>
2024-08-07 14:22:18 -0600

    feat: Changing the styles and adding a version parameter to the link label in the header of the html

--- Views/bienvenido.php
+++ Views/bienvenido.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/bienvenido.css">
+    <link rel="stylesheet" href="styles/bienvenido.css?v=1.0">
     <title>Bienvenido</title>
 </head>
 
@@ -18,9 +18,9 @@
             require_once('../Controllers/estudianteController.php');
             $estudiantes = EstudianteController::index();
 
-            echo '<h3>Carnet: </h3> ' . $estudiantes[count($estudiantes) - 1]['carnet'] .'<br><br>';
-            echo '<h3>Nombres: </h3> ' . $estudiantes[count($estudiantes) - 1]['nombres'] .'<br><br>';
-            echo '<h3>Apellidos: </h3> ' . $estudiantes[count($estudiantes) - 1]['apellidos'] .'<br><br>';
+            echo '<h3>Carnet </h3> ' . $estudiantes[count($estudiantes) - 1]['carnet'] .'<br><br>';
+            echo '<h3>Nombres </h3> ' . $estudiantes[count($estudiantes) - 1]['nombres'] .'<br><br>';
+            echo '<h3>Apellidos </h3> ' . $estudiantes[count($estudiantes) - 1]['apellidos'] .'<br><br>';
             ?>
         </header>
 
